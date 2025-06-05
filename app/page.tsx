5724e32e0e7c8dbb9b3d4458444def7c62a15b7e
nesrucodex <nesrucodex01@gmail.com>
2025-06-05 23:10:34 +0300

    Removing cursor effect

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Projects from "@/components/projects"
 import About from "@/components/about"
 import Contact from "@/components/contact"
 import Footer from "@/components/footer"
-import CursorEffect from "@/components/cursor-effect"
+// import CursorEffect from "@/components/cursor-effect"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <CursorEffect />
+      {/* <CursorEffect /> */}
       <Hero />
       <Projects />
       <About />
